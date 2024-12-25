8502f63f1711f34ccf3a64c5ff21485efebc2183
MattForn <matthiasfornefeld@gmail.com>
2024-12-25 18:29:25 +0100

    Fall saves and load models

--- SAC_try.py
+++ SAC_try.py
@@ -10,6 +10,9 @@ from torchvision.transforms import Compose, Normalize, ToTensor, Resize
 import torch
 import torch.nn.functional as F
 from torchvision.models import convnext_base
+from dm_control.rl.control import PhysicsError
+import os
+import glob
 
 # Custom policy
 from stable_baselines3.sac.policies import SACPolicy
@@ -21,13 +24,68 @@ from typing import Any, Dict, List, Optional, Type, Union
 env = gym.make("gym_aloha/AlohaInsertion-features-v0")
 
 observation, info = env.reset()
-print(type(observation["top"]))
-print(np.shape(observation["top"]))
 
-model = stable_baselines3.SAC("MultiInputPolicy", env, verbose=1, buffer_size=2**21, batch_size=2**12)
+batch_size = 256
+#load the last saved model in models with the graetest amounts of steps
+try:
+    #find the last saved model
+    print('trying to load a model')
+    directory = "models/*"
+    list_of_files = glob.glob(directory)
+    list_of_files_zip = [file for file in list_of_files if file.endswith('.zip')]
+    list_of_files_pkl = [file for file in list_of_files if file.endswith('.pkl')]
+    latest_zip_file = max(list_of_files_zip, key=os.path.getctime)
+    latest_pkl_file = max(list_of_files_pkl, key=os.path.getctime)
+    model = stable_baselines3.SAC.load(latest_zip_file, env=env, verbose=1)
+    print('------- successfully loaded Model -------')
+        
+    #chainge batch size of the model
+    model.batch_size = batch_size
+except:
+    print('------- can not loaded Model -------')
+    model = stable_baselines3.SAC("MultiInputPolicy", env, verbose=1, buffer_size=2**21, batch_size=batch_size)
 
-model.learn(total_timesteps=3600, callback=CheckpointCallback(save_freq=1000, save_path='./models/', name_prefix='sac_ConvNext_aloha'))
+try:
 
+    # Load the replay buffer
+    if os.path.exists(latest_pkl_file):
+        print("trying to load replay buffer")
+        with open(latest_pkl_file, 'rb') as f:
+            model.load_replay_buffer(f)
+        print('------- successfully loaded Replay Buffer -------')
+    else:
+        print('------- Replay Buffer not found -------')
+except Exception as e:
+        print('------- cant load Replay Buffer -------')
+        print(e)
+    
+    
+
+# model.learn(total_timesteps=10000, callback=CheckpointCallback(save_freq=1000, save_path='./models/', name_prefix='sac_ConvNext_aloha'))
+# Custom callback to catch errors during learning
+class ErrorCatchingCallback(CheckpointCallback):
+    def __init__(self, save_freq: int, save_path: str, name_prefix: str, save_replay_buffer=True, del_old_checkpoints=True):
+        super().__init__(save_freq, save_path, name_prefix, save_replay_buffer, del_old_checkpoints=del_old_checkpoints)
+
+    def _on_step(self) -> bool:
+        try:
+            return super()._on_step()
+        except Exception as e:
+            
+            print("################################")
+            if e == PhysicsError:
+                print(f"PhysicsError: {e}")
+            else:
+                print(f"Error during learning: {e}")
+            return False
+
+# Use the custom callback
+ecc = ErrorCatchingCallback(save_freq=10000, save_path='./models/',
+                                                name_prefix='sac_ConvNext_aloha',
+                                                save_replay_buffer=True,
+                                                del_old_checkpoints=True)
+print("starting to learn")
+model.learn(total_timesteps=1000000, callback=ecc)
 # save the model
 model.save("sac_ConvNext_aloha")
 
@@ -73,7 +131,7 @@ for i in range(100):
         observation, info = env.reset()
 
 env.close()
-filename = "examples/example" + str(model._total_timesteps) + ".mp4"
+filename = "videos/example" + str(model._total_timesteps) + ".mp4"
 imageio.mimsave(filename, np.stack(frames), fps=25)
 
 
