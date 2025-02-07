ea84fbb81ea65159c8d9d613138b0a38b305fa0d
ppaetz <ppaetz@stud.hs-bremen.de>
2025-02-07 13:42:50 +0100

    diesdas

--- gym__aloha/gym_aloha/__init__.py
+++ gym__aloha/gym_aloha/__init__.py
@@ -25,7 +25,7 @@ register(
     entry_point="gym_aloha.env:AlohaEnv",
     max_episode_steps=300,
     nondeterministic=True,
-    kwargs={"obs_type": "pixels", "task": "simple"},
+    kwargs={"obs_type": "pixels", "task": "transfer_cube"},
 )
 
 register(
