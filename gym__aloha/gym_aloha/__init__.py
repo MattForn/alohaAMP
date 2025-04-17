5d70cad166b8534a03bee682b9ef3bc3dad084e5
ppaetz <pit.paetz@uni-bielefeld.de>
2025-04-17 11:30:46 +0200

    changed max_steps

--- gym__aloha/gym_aloha/__init__.py
+++ gym__aloha/gym_aloha/__init__.py
@@ -22,9 +22,8 @@ register(
 
 register(
     id="gym_aloha/AlohaSimple",
-    #entry_point="gym_aloha.env:AlohaEnv",
     entry_point="gym_aloha.env:SimpleAlohaEnv",
-    max_episode_steps=300,
+    max_episode_steps=150,
     nondeterministic=True,
     kwargs={"obs_type": "agent_pos", "task": "simple"},
 )
